1b2e444fc8a7c59466ec83eb42c03d71b885a689
mshipan <mshipan657@gmail.com>
2023-05-19 22:56:30 +0600

    update functions for view details button

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -29,7 +29,18 @@ async function run() {
 
     const toyCollection = client.db("toyDB").collection("toys");
 
-    app.get("/toys", async (req, res) => {});
+    app.get("/toys", async (req, res) => {
+      const cursor = toyCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/toy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.findOne(query);
+      res.send(result);
+    });
 
     app.post("/toys", async (req, res) => {
       const newToy = req.body;

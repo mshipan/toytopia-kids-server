91b6c85e4f45097439473ef1e756aa6228a8e50d
mshipan <mshipan657@gmail.com>
2023-05-20 15:08:59 +0600

    update delete functionality

--- index.js
+++ index.js
@@ -57,6 +57,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/toy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(

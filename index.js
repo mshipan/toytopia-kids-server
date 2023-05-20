bb1d1b81b759b725fd6113c9cf15500ada20f88e
mshipan <mshipan657@gmail.com>
2023-05-20 18:15:32 +0600

    update update toy functionality

--- index.js
+++ index.js
@@ -50,6 +50,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/updatetoy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/toys", async (req, res) => {
       const newToy = req.body;
       console.log(newToy);

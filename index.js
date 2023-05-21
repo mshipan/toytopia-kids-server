e55bc4be8d6c47f97c21943998c0ed580dae79bc
mshipan <mshipan657@gmail.com>
2023-05-21 15:08:37 +0600

    fix cors policy bug

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 const corsConfig = {
-  origin: "",
+  origin: "https://toytopia-kids.web.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };

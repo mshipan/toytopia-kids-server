ce64f9fe2306edd17cc117a0f533ee760661412a
mshipan <mshipan657@gmail.com>
2023-05-21 13:58:37 +0600

    fix problems

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+const corsConfig = {
+  origin: "",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsConfig));
+app.options("", cors(corsConfig));
 app.use(express.json());
 
 //mongodb code start
